51596fbb94cdb6a17b70eaf26040ab09989d7c7c
Joffy Jose <joffy.jose.jfy@gmail.com>
2022-03-11 17:46:00 +0530

    url moved to app props

--- src/main/java/com/jfy/springfeignclientdemo/api/UserClient.java
+++ src/main/java/com/jfy/springfeignclientdemo/api/UserClient.java
@@ -1,12 +1,14 @@
 package com.jfy.springfeignclientdemo.api;
 
 import com.jfy.springfeignclientdemo.dto.UserSchema;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 
 import java.util.List;
 
-@FeignClient(url = "https://jsonplaceholder.typicode.com/" , name = "USER")
+@FeignClient(url = "${feign.client.url}" , name = "USER")
+//@FeignClient(url = "https://jsonplaceholder.typicode.com/" , name = "USER")
 public interface UserClient {
     @GetMapping("/users")
     public List<UserSchema> getUsers();
