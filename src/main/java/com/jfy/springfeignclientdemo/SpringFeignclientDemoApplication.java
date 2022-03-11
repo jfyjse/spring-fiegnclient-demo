0d7a6785089d5d9c5a453348ec1a665b0bdbd0d8
Joffy Jose <joffy.jose.jfy@gmail.com>
2022-03-11 10:23:21 +0530

    first

---
+++ src/main/java/com/jfy/springfeignclientdemo/SpringFeignclientDemoApplication.java
@@ -0,0 +1,13 @@
+package com.jfy.springfeignclientdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringFeignclientDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringFeignclientDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jfy/springfeignclientdemo/SpringFeignclientDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jfy.springfeignclientdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringFeignclientDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
